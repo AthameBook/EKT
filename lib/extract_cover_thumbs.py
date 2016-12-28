ee009383254f81a03eb6cb74369c9de1457b27aa
AthameBook <git@github.com:AthameBook>
2016-12-29 00:09:30 +0100

    modified:   lib/extract_cover_thumbs.py

--- lib/extract_cover_thumbs.py
+++ lib/extract_cover_thumbs.py
@@ -5,9 +5,9 @@
 # GNU Affero GPLv3 or later.
 # Copyright © Robert Błaut.
 
-import os
 from __future__ import unicode_literals
 import sqlite3
+import os
 from imghdr import what
 from io import BytesIO
 from datetime import datetime
@@ -17,12 +17,15 @@ from lib.pages import find_exth
 from lib.pages import get_pages
 from lib.kfxmeta import get_kindle_kfx_metadata
 from lib.dualmetafix import DualMobiMetaFix
+
 from PIL import Image
 
-tablica = 'Entries'
+SFENC = sys.getfilesystemencoding()
+
+tablica = 'DeviceContentEntry'
 polozenie = 'p_location'
 miniatura = 'p_thumbnail'
-bazacc = '/var/local/cc.db'
+baza = '/var/local/dcm.db'
 
 def get_cover_image(section, mh, metadata, file, fide):
     try:
@@ -39,7 +42,7 @@ def get_cover_image(section, mh, metadata, file, fide):
                        "FONT", "RESC"]:
             imgnames.append(None)
             continue
-        if data == chr(0xe9) + chr(0x8e) + "\r\n":
+        if data == chr(0xe9) + chr(0x8e) + "\r\n".encode('UTF-8'):
             imgnames.append(None)
             continue
         imgtype = what(None, data)
@@ -63,30 +66,14 @@ def process_image(data):
     cover = cover.convert('L')
     return cover
 
-def generate_apnx_files(docs, days):
+def generate_apnx_files(docs):
     apnx_builder = APNXBuilder()
-    if days is not None:
-        dtt = datetime.today()
-        days_int = int(days)
-    else:
-        days_int = 0
-        diff = 0
     for root, dirs, files in os.walk(docs):
         for name in files:
-            if 'documents' + os.path.sep + 'dictionaries' in root:
-                continue
-            mobi_path = os.path.join(root, name)    
+            mobi_path = os.path.join(root, name)
             if '.sdr' in root:
                 continue
-            if days is not None:
-                try:
-                    dt = os.path.getctime(mobi_path)
-                except OSError:
-                    continue
-                dt = datetime.fromtimestamp(dt).strftime('%Y-%m-%d')
-                dt = datetime.strptime(dt, '%Y-%m-%d')
-                diff = (dtt - dt).days
-            if name.lower().endswith(('.azw3', '.mobi', '.azw')) and diff <= days_int:
+            if name.lower().endswith(('.azw3', '.mobi', '.azw')):
                 sdr_dir = os.path.join(root, os.path.splitext(
                                        name)[0] + '.sdr')
                 if not os.path.isdir(sdr_dir):
@@ -98,42 +85,37 @@ def generate_apnx_files(docs, days):
                         continue
                     apnx_builder.write_apnx(mobi_path, apnx_path)
 
-def extract_cover_thumbs(kindlepath, days):
+def extract_cover_thumbs(kindlepath):
     docs = os.path.join(kindlepath, 'documents')
-    if days is not None:
-        dtt = datetime.today()
-        days_int = int(days)
-    else:
-        days_int = 0
-        diff = 0
-
     if not os.path.isdir(os.path.join(kindlepath, 'system', 'thumbnails')):
         return 1
     extensions = ('.azw', '.azw3', '.mobi', '.kfx', '.azw8')
-    for root, dirs, files in os.walk(docs):
-        for name in files:
-            if '.sdr' in root:
-                continue
-            if days is not None:
-                try:
-                    dt = os.path.getctime(os.path.join(root, name))
-                except OSError:
-                    continue
-                dt = datetime.fromtimestamp(dt).strftime('%Y-%m-%d')
-                dt = datetime.strptime(dt, '%Y-%m-%d')
-                diff = (dtt - dt).days
-            if name.lower().endswith(extensions) and diff <= days_int:
+
+    conn = sqlite3.connect(baza)
+    c = conn.cursor()
+
+    c.execute('SELECT {cn}, {coi} FROM {tn} WHERE {coi} IS NOT NULL AND {coi} != "0"'.\
+        format(coi=miniatura, tn=tablica, cn=polozenie))
+    files = c.fetchall()
+
+    for names in files:
+        name = names[0]
+        nameonly = os.path.basename(name)
+        thumbpath = names[1]
+
+        #Dodatkowy warunek -- działaj tylko dla istniejących plików na dysku/czytniku (to pewnie można będzie usunąć po testach)
+        if os.path.isfile(name):
+            if name.lower().endswith(extensions):
                 if name.lower().endswith('.kfx') or name.lower().endswith('.azw8'):
                     is_kfx = True
                 else:
                     is_kfx = False
                 fide = name.decode('UTF-8')
-                mobi_path = os.path.join(root, name)
                 if is_kfx:
                     if '_sample' in fide:
                         continue
                     try:
-                        kfx_metadata = get_kindle_kfx_metadata(mobi_path)
+                        kfx_metadata = get_kindle_kfx_metadata(name)
                     except Exception as e:
                         continue
                     doctype = kfx_metadata.get("cde_content_type")
@@ -141,13 +123,14 @@ def extract_cover_thumbs(kindlepath, days):
                         continue
                     asin = kfx_metadata.get("ASIN")
                 else:
-                    if '!DeviceUpgradeLetter!' in fide:
+                    #Ten warunek też raczej do usunięcia, bo w bazie i tak nie będzie okładki
+                    if '!DeviceUpgradeLetter!' in name:
                         continue
-                    with open(mobi_path, 'rb') as mf:
+                    with open(name, 'rb') as mf:
                         mobi_content = mf.read()
                         if mobi_content[60:68] != 'BOOKMOBI':
                             continue
-                    section = kindle_unpack.Sectionizer(mobi_path)
+                    section = kindle_unpack.Sectionizer(name)
                     mhlst = [kindle_unpack.MobiHeader(section, 0)]
                     mh = mhlst[0]
                     metadata = mh.getmetadata()
@@ -159,24 +142,7 @@ def extract_cover_thumbs(kindlepath, days):
                         doctype = metadata['Document Type'][0]
                     except KeyError:
                         doctype = None
-                if asin is None:
-                    conn = sqlite3.connect(bazacc)
-                    c = conn.cursor()
-
-                    c.execute('SELECT ({coi}) FROM {tn} WHERE {cn}="{fn}"'.\
-                        format(coi=miniatura, tn=tablica, cn=polozenie, fn=mobi_path))
-                    wynik = c.fetchone()
-                    try:
-                        thumbpath = wynik[0]
-                    except TypeError:
-                        thumbpath = None
-                    conn.close()
-                    if thumbpath is None:
-                        continue
-                thumbpath = os.path.join(
-                    kindlepath, 'system', 'thumbnails',
-                    'thumbnail_%s_%s_portrait.jpg' % (asin, doctype)
-                )
+                conn.close()
                 if (not os.path.isfile(thumbpath)):
                     if is_kfx:
                         image_data = kfx_metadata.get("cover_image_data")
@@ -186,12 +152,12 @@ def extract_cover_thumbs(kindlepath, days):
                         if is_kfx:
                             cover = process_image(image_data.decode('base64'))
                         else:
-                            cover = get_cover_image(section, mh, metadata, name, fide)
+                            cover = get_cover_image(section, mh, metadata, name, nameonly)
                     except IOError:
                         continue
                     if not cover:
                         continue
-                    cover.save(thumbpath)                        
-    generate_apnx_files(docs, days)
+                    cover.save(thumbpath)
+    generate_apnx_files(docs)
 
     return 0
